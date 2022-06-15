d4c5cfe3f70b7a5643e1391fa3d8557167ec6409
YEGON <kptooman@gmail.com>
2022-06-15 17:30:36 +0300

    removing generator token

--- src/main/java/com/springboot/blog/security/JwtTokenProvider.java
+++ src/main/java/com/springboot/blog/security/JwtTokenProvider.java
@@ -14,10 +14,5 @@ public class JwtTokenProvider {
     @Value("${app.jwt-expiration-milliseconds}")
     private int jwtExpirationInMs;
 
-     generate token
-    public  String generateToken(Authentication authentication){
-        String username = authentication.getName();
-        Date currentDate = new Date();
-        Date expireDate = new Date(currentDate.getTime() + jwtExpirationInMs
-   }
+   
 }
